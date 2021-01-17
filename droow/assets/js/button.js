9cf2539aa2be9aefffa0b828d76ad3bf2038fcf0
rubanik00 <geriys12@gmail.com>
2021-01-17 17:59:41 +0200

    done

--- droow/assets/js/button.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function (){
-    $("#playBut").click(function (){
-        console.log('click')
-        $('html, body').animate({
-            scrollTop: $("#sdfasfasdf").offset().top
-        }, 1000);
-    });
-});
